a20e4706de5808e2c8876e841281368009d384f9
Marcholio <marcholio09@gmail.com>
2017-01-06 16:35:09 +0200

    Combined react and express node

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-import express from 'express';
+const express= require('express');
 
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 4040;
 
 // Path to built content folder
-const content_path = path.join(__dirname, './../app');
+const content_path = path.join(__dirname, './../build');
 
 app.get('/', (req, res) => {
   res.header('Cache-Control', "max-age=60, must-revalidate, private");
