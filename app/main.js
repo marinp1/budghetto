0cec45f6ac164ab7dd49e4ee2587ff2c46a46473
Marcholio <marcholio09@gmail.com>
2017-01-06 17:20:03 +0200

    Fixed App import to work on ES6

--- app/main.js
+++ app/main.js
@@ -3,7 +3,7 @@ require("./Assets/style.scss");
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-const App = require('./App.js');
+import App from './App.js';
 const AppElem = React.createElement(App);
 
 ReactDOM.render(AppElem, document.getElementById('app'));
