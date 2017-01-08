5434df968202fdcc44fead42542d2b6744b1d189
Patrik Marin <me@patrikmarin.fi>
2017-01-08 12:39:16 +0200

    Changed table dropping to force sync

--- init-scripts/import-test-data.js
+++ init-scripts/import-test-data.js
@@ -4,21 +4,29 @@ const path = require('path');
 const sequelize_fixtures = require('sequelize-fixtures');
 const testDataPath = path.join(__dirname, "test-data.json");
 
+let sequelize;
+const Sequelize = require('sequelize');
+
+// Select correct database
+if ( process.env.DATABASE_URL != undefined ) {
+  sequelize = new Sequelize( process.env.DATABASE_URL );
+} else {
+  sequelize = new Sequelize('sequelize', '', '', {
+   dialect: 'sqlite',
+   storage: path.join(__dirname, '../dev-resources/data.sqlite'),
+   logging: false
+  });
+}
+
 module.exports = {
   importData: function importData(models) {
 
     return new Promise(function (resolve, reject) {
 
-      models.UserAccount.drop().then(function() {
-        models.BankAccount.drop();
-      }).then(function() {
-        models.Category.drop();
-      }).then(function() {
-        models.Transaction.drop();
-      }).then(function() {
+      sequelize.sync({force: true}).then(function(){
         resolve(sequelize_fixtures.loadFile(testDataPath, models));
       });
-      
+
     });
   }
 };
