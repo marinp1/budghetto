ce1399262314d91bdb1326f28cb3d3c6d15dab5b
Patrik Marin <patrik.marin@gmail.com>
2017-01-08 15:32:35 +0200

    Removed additional sequelize calls
    Use db object from models

--- init-scripts/import-test-data.js
+++ init-scripts/import-test-data.js
@@ -4,38 +4,14 @@ const path = require('path');
 const sequelize_fixtures = require('sequelize-fixtures');
 const testDataPath = path.join(__dirname, "test-data.json");
 
-let sequelize;
-const Sequelize = require('sequelize');
-
-// Select correct database
-if ( process.env.DATABASE_URL != undefined ) {
-  sequelize = new Sequelize( process.env.DATABASE_URL );
-} else {
-  sequelize = new Sequelize('sequelize', '', '', {
-   dialect: 'sqlite',
-   storage: path.join(__dirname, '../dev-resources/data.sqlite'),
-   logging: false
-  });
-}
 module.exports = {
   importData: function importData(models) {
 
     return new Promise(function (resolve, reject) {
 
-      // Ghetto solution, try to fix if possible
-      try {
-        models.UserAccount.drop().then(function() {
-          models.BankAccount.drop();
-        }).then(function() {
-          models.Category.drop();
-        }).then(function() {
-          models.Transaction.drop();
-        }).then(function() {
-          resolve(sequelize_fixtures.loadFile(testDataPath, models));
-        });
-      } catch (err) {
+      models.sequelize.sync({force: true}).then(function() {
         resolve(sequelize_fixtures.loadFile(testDataPath, models));
-      }
+      });
 
     });
   }
