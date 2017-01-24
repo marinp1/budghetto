9d9fc7d12bb75ccd673de1374d63b9e40eeead45
Marcholio <marcholio09@gmail.com>
2017-01-24 19:42:27 +0200

    Mobile header ready

--- app/App.js
+++ app/App.js
@@ -10,30 +10,43 @@ import TransactionView from './TransactionView.js';
 class Header extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = { mobileMenu: false };
+    this.hideMobileMenu = this.hideMobileMenu.bind(this);
+  }
+
+  logout() {
+    globals.loggedInUserId = '';
+    browserHistory.push('/');
+  }
+
+  toggleMobileMenu() {
+    this.setState({ mobileMenu: !this.state.mobileMenu });
+  }
+
+  // Used to hide menu when user clicks navigation element
+  hideMobileMenu() {
+    this.setState({ mobileMenu: false });
   }
 
   render() {
     return (
       <header>
         <h1>Budghetto</h1>
-        <div id="nav-elems">
-          <NavElem text='Transactions' changeView={ this.props.changeView } className={ this.props.currentView == 'Transactions' ? ' selected' : ''} icon={ <FontAwesome name='exchange' /> } />
-          <NavElem text='Menu2' changeView={ this.props.changeView } className={ this.props.currentView == 'Transactions2' ? ' selected' : ''} icon={ <FontAwesome name='bell' /> } />
-        </div>
-        <div id="loggedInAs">
-          <div>
-            <p id="logged-text">Logged in as:</p>
-            <p>{ globals.loggedInUserId }</p>
+          <div id="nav-elems" className={ this.state.mobileMenu ? 'mobile-visible' : 'mobile-hidden'}>
+            <NavElem text='Transactions' changeView={ this.props.changeView } hideMobileMenu={ this.hideMobileMenu } className={ this.props.currentView == 'Transactions' ? ' selected' : ''} icon={ <FontAwesome name='exchange' /> } />
+            <NavElem text='Menu2' changeView={ this.props.changeView } hideMobileMenu={ this.hideMobileMenu } className={ this.props.currentView == 'Menu2' ? ' selected' : ''} icon={ <FontAwesome name='bell' /> } />
+            <div id="loggedInAs" className={ this.state.mobileMenu ? 'mobile-visible' : 'mobile-hidden'}>
+              <div>
+                <p id="logged-text">Logged in as:</p>
+                <p className="mobile-hidden">{ globals.loggedInUserId }</p>
+              </div>
+              <button id="logoutButton" onClick={ () => this.logout() }>Log out</button>
+            </div>
           </div>
-          <button id="logoutButton" onClick={ () => this.logout() }>Log out</button>
-        </div>
+        <FontAwesome name="bars" id="mobile-menu" onClick={ () => this.toggleMobileMenu() }/>
       </header>
     );
-  }
-
-  logout() {
-    globals.loggedInUserId = '';
-    browserHistory.push('/');
   }
 }
 
@@ -44,7 +57,7 @@ class NavElem extends React.Component {
 
   render() {
     return (
-      <div onClick={ () => this.props.changeView(this.props.text) } className={'nav' + this.props.className }>
+      <div onClick={ () => { this.props.changeView(this.props.text); this.props.hideMobileMenu(); } } className={'nav' + this.props.className }>
         { this.props.icon }
         <p>{ this.props.text }</p>
       </div>
